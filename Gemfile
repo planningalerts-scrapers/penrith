8842673a40795eda29558b2165a4db6033dd6d90
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 03:44:49 +1000

    Switch over to using gem

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source "https://rubygems.org"
 
+# gem "icon_scraper", git: "https://github.com/planningalerts-scrapers/icon_scraper.git"
+gem "icon_scraper", path: "../icon_scraper"
 gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
-gem "mechanize"
 
 gem "vcr"
 gem "webmock"
--- scraper.rb
+++ scraper.rb
@@ -1,38 +1,10 @@
-require 'scraperwiki'
-require 'rss/2.0'
-require 'date'
-require 'mechanize'
+require "icon_scraper"
 
-url = 'http://bizsearch.penrithcity.nsw.gov.au/ePlanning/Pages/XC.Track/SearchApplication.aspx?d=thismonth&k=LodgementDate&t=DA,DevApp&o=rss'
-
-agent = Mechanize.new
-
-# For some incomprehensible reason, there's an "I agree" page for an RSS feed.
-page = agent.get(url)
-form = page.forms.first
-form.checkbox_with(:name => /Agree/).check
-page2 = form.submit(form.button_with(:name => /Agree/))
-
-t = page2.content.to_s
-
-# I've no idea why the RSS feed says it's encoded as utf-16 when as far as I can tell it isn't
-# Hack it by switching it back to utf-8
-t.gsub!("utf-16", "utf-8")
-
-
-# Now we can fetch the data
-feed = RSS::Parser.parse(t, false)
-
-feed.channel.items.each do |item|
-  record = {
-    'council_reference' => item.title.split(' ')[0],
-    'description'       => item.description.split('.')[1].strip,
-    # Have to make this a string to get the date library to parse it
-    'date_received'     => Date.parse(item.pubDate.to_s),
-    'address'           => item.description.split('.')[0].strip,
-    'date_scraped'      => Date.today.to_s,
-    'info_url'          => "http://bizsearch.penrithcity.nsw.gov.au/ePlanning/Pages/XC.Track/SearchApplication.aspx#{item.link}"
-  }
-  # p record
-  ScraperWiki.save_sqlite(['council_reference'], record)
+IconScraper.scrape_with_params(
+  url: "http://bizsearch.penrithcity.nsw.gov.au/ePlanning/Pages",
+  period: "thismonth",
+  types: ["DA", "DevApp"]
+) do |record|
+  record["address"] = record["address"].gsub(",", "")
+  IconScraper.save(record)
 end
